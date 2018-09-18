6867de4d08fe5bfb6be3f00d5abadac58f6f40d4
Sadouioui El Khamis <el-khamis.sadouioui@etud.univ-montp2.fr>
2018-09-18 12:42:33 +0200

    Test

--- mobile/src/main/java/com/example/alachguer/tp1/MainActivity.kt
+++ mobile/src/main/java/com/example/alachguer/tp1/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Ici c'est la branche des features DEV
     }
 }
