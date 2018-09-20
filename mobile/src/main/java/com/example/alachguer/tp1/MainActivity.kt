1804a6d8d0baedc56bf9af2b4dd9f9f2afb39792
El-khamis <kamss34@hotmail.fr>
2018-09-20 11:44:13 +0200

    Bottom view navigation done Tr

--- mobile/src/main/java/com/example/alachguer/tp1/MainActivity.kt
+++ mobile/src/main/java/com/example/alachguer/tp1/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.alachguer.tp1
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity.*
 
 class MainActivity : AppCompatActivity() {
 
+//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.navigation_home -> {
+//                message.setText(R.string.title_home)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_dashboard -> {
+//                message.setText(R.string.title_dashboard)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_notifications -> {
+//                message.setText(R.string.title_notifications)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //Ici c'est la branche des features DEV
+        setContentView(R.layout.activity)
+
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
