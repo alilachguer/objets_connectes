854b1ceeff2db199f4411d0a6cdb373d00ba3b86
alilachguer <alilachguer95@gmail.com>
2018-10-09 10:27:26 +0200

    update add task view

--- mobile/src/main/java/com/example/alachguer/tp1/AddTaskFragment.kt
+++ mobile/src/main/java/com/example/alachguer/tp1/AddTaskFragment.kt
@@ -1,4 +1,6 @@
 package com.example.alachguer.tp1
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker
 import android.widget.EditText
+import kotlinx.android.synthetic.main.fragment_add_task.*
 
 class AddTaskFragment : Fragment(){
 
     lateinit var editText: EditText
     lateinit var datePicker: DatePicker
+    lateinit var titleInput: EditText
+    lateinit var descriptionInput: EditText
 
     companion object {
         val TITLE_EXTRA: String = "title_value"
@@ -25,11 +30,26 @@ class AddTaskFragment : Fragment(){
         datePicker = view.findViewById(R.id.date)
 
         editText.setOnClickListener{
-            val descriptionActivity = Intent(context, TaskActivity::class.java)
-            descriptionActivity.apply {
-                putExtra(TITLE_EXTRA, editText.text)
-            }
-            startActivity(descriptionActivity)
+            var li: LayoutInflater = LayoutInflater.from(context)
+            var promptsView: View = li.inflate(R.layout.activity_task, null)
+            var alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
+
+            alertDialogBuilder.setView(promptsView)
+
+            var titleInput: EditText = promptsView.findViewById(R.id.desc_title);
+            var descriptionInput: EditText = promptsView.findViewById(R.id.desc_description);
+
+            alertDialogBuilder.setCancelable(false)
+                    .setPositiveButton("OK"){
+                        dialog, whichbutton ->
+                            titre.setText(titleInput.text)
+                    }
+                    .setNegativeButton("Cancel"){
+                        dialog, whichbutton -> dialog.cancel()
+                    }
+
+            var alertDialog: AlertDialog = alertDialogBuilder.create()
+            alertDialog.show()
         }
 
 
