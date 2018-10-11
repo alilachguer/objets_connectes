6127b5447fb9a93c068f9dc169bb0f3155ca5097
alilachguer <alilachguer95@gmail.com>
2018-10-11 18:03:54 +0200

    un commit with comilation error

--- mobile/src/main/java/com/example/alachguer/tp1/TodoModel.kt
+++ mobile/src/main/java/com/example/alachguer/tp1/TodoModel.kt
@@ -2,11 +2,21 @@ package com.example.alachguer.tp1
 
 import java.util.*
 
-class TodoModel(
-        val todoId: Int,
-        val title: String,
-        val description: String,
-        val date: String,
-        val timeHour: Int,
-        val timeMinute: Int,
-        val notification: Int)
+class TodoModel
+{
+    constructor(
+            todoId: Int,
+            title: String,
+            description: String,
+            date: String,
+            timeHour: Int,
+            timeMinute: Int,
+            notification: Int)
+
+    constructor(title: String,
+                description: String,
+                date: String,
+                timeHour: Int,
+                timeMinute: Int,
+                notification: Int)
+}
